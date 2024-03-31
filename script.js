a620d332f1c71db0480636472e65fc8a085daee3
scifigurmeet <56145753+scifigurmeet@users.noreply.github.com>
2024-03-31 17:09:41 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Add JavaScript functionality here
+console.log("Script loaded!");
+
+// Example console output
+console.log("This is a console log message.");
+
+// You can add more console logs as needed
